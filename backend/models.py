dd0af94412749a892353c35af6d905766e82516d
Maria Daniela Martin <m.danielamartin@proton.me>
2025-06-01 00:46:18 -0400

    login

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta, timezone
+
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+
 class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(255), nullable=False)
-    status = db.Column(db.String(50), default="open")
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=timezone.utc)
+    status = db.Column(db.Enum('draft', 'published', 'completed', name="status"), default='draft')
+    publish_date=db.Column(db.DateTime, nullable=True)
+    time_limit_days = db.Column(db.Integer, default=30)
+
+    def is_expired(self):
+        if self.status == 'published' and timezone.utc > self.publish_date + timedelta(days=self.time_limit_days):
+            self.status = 'completed'
+            db.session.commit()
+            return True
+        return False
---
+++ backend/utils.py
@@ -0,0 +1,10 @@
+from .models import User
+
+
+def check_user(id):
+    if not id:
+        return False
+    user = User.query.get(id=id)
+    if not user:
+        return False
+    return True
