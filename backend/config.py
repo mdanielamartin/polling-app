cb93d221cae955b778a5514e9582b1e4afbceb3e
Maria Daniela Martin <m.danielamartin@proton.me>
2025-09-08 01:19:38 +0000

    switch db env

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://mdmpereira:nina@db:5432/poll_app")
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://mdmpereira:nina@db:5432/poll_app"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
